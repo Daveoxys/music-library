64ea1a78016e5c247c0daec6216ac6935e76fa52
Dave Taylor <davidtaylor970@googlemail.com>
2022-01-06 16:39:30 +0000

    Database setup

--- src/app.js
+++ src/app.js
@@ -2,4 +2,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send("Hello? Is there anybody in there? Just nod if you can hear me.");
+});
+
 module.exports = app;
